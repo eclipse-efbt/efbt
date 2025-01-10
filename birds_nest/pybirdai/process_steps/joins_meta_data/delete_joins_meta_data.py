29c99fe541a6ee901483ce3f7d959383a1ec0c93
RBirdwatcher <neil@birdsoftwaresolutions.com>
2025-01-10 17:32:34 +0000

    error repeating the process in Github codespaces #1480
    https://github.com/eclipse-efbt/efbt/issues/1480

--- birds_nest/pybirdai/process_steps/joins_meta_data/delete_joins_meta_data.py
+++ birds_nest/pybirdai/process_steps/joins_meta_data/delete_joins_meta_data.py
@@ -10,7 +10,7 @@
 # Contributors:
 #    Neil Mackenzie - initial API and implementation
 #
-
+from pybirdai.context.sdd_context_django import SDDContext
 from pybirdai.bird_meta_data_model import *
 from django.apps import apps
 from django.db.models.fields import CharField,DateTimeField,BooleanField,FloatField,BigIntegerField
@@ -73,49 +73,95 @@ class TransformationMetaDataDestroyer:
         COMBINATION_ITEM.objects.all().delete()
         CUBE_TO_COMBINATION.objects.all().delete()
 
-        context.mapping_definition_dictionary = {}
-        context.variable_mapping_dictionary = {}
-        context.variable_mapping_item_dictionary = {}
-        context.bird_cube_structure_dictionary = {}
-        context.bird_cube_dictionary = {}
-        context.bird_cube_structure_item_dictionary = {}
-        context.mapping_to_cube_dictionary = {}
-        context.agency_dictionary = {}
-        context.framework_dictionary = {}
-        context.domain_dictionary = {}
-        context.member_dictionary = {}
-        context.member_id_to_domain_map = {}
-        context.member_id_to_member_code_map = {}
-        context.variable_dictionary = {}
-        context.variable_to_domain_map = {}
-        context.variable_to_long_names_map = {}
-        context.variable_to_primary_concept_map = {}
-        context.member_hierarchy_dictionary = {}
-        context.member_hierarchy_node_dictionary = {}
-        context.report_tables_dictionary = {}
-        context.axis_dictionary = {}
-        context.axis_ordinate_dictionary = {}
+        sdd_context.mapping_definition_dictionary = {}
+        sdd_context.variable_mapping_dictionary = {}
+        sdd_context.variable_mapping_item_dictionary = {}
+        sdd_context.bird_cube_structure_dictionary = {}
+        sdd_context.bird_cube_dictionary = {}
+        sdd_context.bird_cube_structure_item_dictionary = {}
+        sdd_context.mapping_to_cube_dictionary = {}
+        sdd_context.agency_dictionary = {}
+        sdd_context.framework_dictionary = {}
+        sdd_context.domain_dictionary = {}
+        sdd_context.member_dictionary = {}
+        sdd_context.member_id_to_domain_map = {}
+        sdd_context.member_id_to_member_code_map = {}        
+        sdd_context.variable_dictionary = {}                
+        sdd_context.variable_to_domain_map = {}
+        sdd_context.variable_to_long_names_map = {}
+        sdd_context.variable_to_primary_concept_map = {}
+        sdd_context.member_hierarchy_dictionary = {}
+        sdd_context.member_hierarchy_node_dictionary = {}
+        sdd_context.report_tables_dictionary = {}
+        sdd_context.axis_dictionary = {}
+        sdd_context.axis_ordinate_dictionary = {}
         sdd_context.axis_ordinate_to_ordinate_items_map = {}
-        context.table_cell_dictionary = {}
-        context.table_to_table_cell_dictionary = {}
-        context.cell_positions_dictionary = {}
-        context.member_mapping_dictionary = {}
-        context.member_mapping_items_dictionary = {}
-        context.combination_item_dictionary = {}
-        context.combination_dictionary = {}
-        context.combination_to_rol_cube_map = {}
-        context.cube_link_dictionary = {}
-        context.cube_link_to_foreign_cube_map = {}
-        context.cube_link_to_join_identifier_map = {}
-        context.cube_link_to_join_for_report_id_map = {}
-        context.cube_structure_item_links_dictionary = {}
-        context.cube_structure_item_link_to_cube_link_map = {}
-        context.subdomain_dictionary = {}
-        context.subdomain_to_domain_map ={}
-        context.subdomain_enumeration_dictionary = {}
-        context.members_that_are_nodes = {}
-        context.member_plus_hierarchy_to_child_literals = {}
-        context.domain_to_hierarchy_dictionary = {}
+        sdd_context.table_cell_dictionary = {}
+        sdd_context.table_to_table_cell_dictionary = {}
+        sdd_context.cell_positions_dictionary = {}
+        sdd_context.member_mapping_dictionary = {}
+        sdd_context.member_mapping_items_dictionary = {}
+        sdd_context.combination_item_dictionary = {}
+        sdd_context.combination_dictionary = {}
+        sdd_context.combination_to_rol_cube_map = {}
+        sdd_context.cube_link_dictionary = {}
+        sdd_context.cube_link_to_foreign_cube_map = {}
+        sdd_context.cube_link_to_join_identifier_map = {}
+        sdd_context.cube_link_to_join_for_report_id_map = {}
+        sdd_context.cube_structure_item_links_dictionary = {}
+        sdd_context.cube_structure_item_link_to_cube_link_map = {}
+        sdd_context.subdomain_dictionary = {}
+        sdd_context.subdomain_to_domain_map ={}
+        sdd_context.subdomain_enumeration_dictionary = {}
+        sdd_context.members_that_are_nodes = {}
+        sdd_context.member_plus_hierarchy_to_child_literals = {}
+        sdd_context.domain_to_hierarchy_dictionary = {}
+
+        
+        
+        SDDContext.mapping_definition_dictionary = {}
+        SDDContext.variable_mapping_dictionary = {}
+        SDDContext.variable_mapping_item_dictionary = {}
+        SDDContext.bird_cube_structure_dictionary = {}
+        SDDContext.bird_cube_dictionary = {}
+        SDDContext.bird_cube_structure_item_dictionary = {}
+        SDDContext.mapping_to_cube_dictionary = {}
+        SDDContext.agency_dictionary = {}
+        SDDContext.framework_dictionary = {}
+        SDDContext.domain_dictionary = {}
+        SDDContext.member_dictionary = {}
+        SDDContext.member_id_to_domain_map = {}
+        SDDContext.member_id_to_member_code_map = {}        
+        SDDContext.variable_dictionary = {}        
+        SDDContext.variable_to_domain_map = {}
+        SDDContext.variable_to_long_names_map = {}
+        SDDContext.variable_to_primary_concept_map = {}
+        SDDContext.member_hierarchy_dictionary = {}
+        SDDContext.member_hierarchy_node_dictionary = {}
+        SDDContext.report_tables_dictionary = {}
+        SDDContext.axis_dictionary = {}
+        SDDContext.axis_ordinate_dictionary = {}
+        SDDContext.axis_ordinate_to_ordinate_items_map = {}
+        SDDContext.table_cell_dictionary = {}
+        SDDContext.table_to_table_cell_dictionary = {}
+        SDDContext.cell_positions_dictionary = {}
+        SDDContext.member_mapping_dictionary = {}
+        SDDContext.member_mapping_items_dictionary = {}
+        SDDContext.combination_item_dictionary = {}
+        SDDContext.combination_dictionary = {}
+        SDDContext.combination_to_rol_cube_map = {}
+        SDDContext.cube_link_dictionary = {}
+        SDDContext.cube_link_to_foreign_cube_map = {}
+        SDDContext.cube_link_to_join_identifier_map = {}
+        SDDContext.cube_link_to_join_for_report_id_map = {}
+        SDDContext.cube_structure_item_links_dictionary = {}
+        SDDContext.cube_structure_item_link_to_cube_link_map = {}
+        SDDContext.subdomain_dictionary = {}
+        SDDContext.subdomain_to_domain_map ={}
+        SDDContext.subdomain_enumeration_dictionary = {}
+        SDDContext.members_that_are_nodes = {}
+        SDDContext.member_plus_hierarchy_to_child_literals = {}
+        SDDContext.domain_to_hierarchy_dictionary = {}
 
 
 
