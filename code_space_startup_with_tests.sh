c26ae4bf05dcf3ae43556b4724b9444f1437ad68
benjamin-arfa <33234814+benjamin-arfa@users.noreply.github.com>
2025-06-25 07:58:07 +0000

    changed the configuration of the code_space_startup_with_tests (need to think of the usefulness of this approach) and fixed the path behaviour for the launching of tests. The tests are running on codespaces. As talked about on Monday, the tests for F_05_01 3.0 and datapoint 152589_REF are failing because of the model change (removing the field in the database and only keeping the property). This is fully expected and show the pipeline in working properly. We now need to review the generated filter code to investigate the true meaning of that test failing.

--- code_space_startup_with_tests.sh
+++ code_space_startup_with_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod -R u+w birds_nest/
 cd birds_nest
 pip install --upgrade pip --quiet
 python -m pip install django==5.1.3 --quiet
@@ -7,5 +8,5 @@ python -m pip install pytest==8.3.4 --quiet
 python -m pip install pytest-xdist==3.6.1 --quiet
 python -m pip install ruff==0.9.7 --quiet
 python -m pip install unidecode==1.3.8 --quiet
-python pybirdai/utils/run_tests.py --uv "False" --config-file "tests/configuration_file_tests.json"
+python pybirdai/utils/datapoint_test_run/run_tests.py --uv "False" --config-file "tests/configuration_file_tests.json"
 python manage.py runserver
