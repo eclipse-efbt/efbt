04b9253e7c225b55ffcaa9b0fcd2d2cc759ad422
Benjamin Arfa <benjamin.arfa.pro@gmail.com>
2025-07-07 14:33:52 +0200

    >feat: changes for the step by step in automode

--- birds_nest/pybirdai/admin.py
+++ birds_nest/pybirdai/admin.py
@@ -1,23 +1,10 @@
-# coding=UTF-8
-# Copyright (c) 2024 Bird Software Solutions Ltd
-# This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License 2.0
-# which accompanies this distribution, and is available at
-# https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-#
-# Contributors:
-#    Neil Mackenzie - initial API and implementation
-
-
 from django.contrib import admin
-
-
-from .bird_meta_data_model import DOMAIN
-admin.site.register(DOMAIN)
 from .bird_meta_data_model import SUBDOMAIN
 admin.site.register(SUBDOMAIN)
+from .bird_meta_data_model import SUBDOMAIN_ENUMERATION
+admin.site.register(SUBDOMAIN_ENUMERATION)
+from .bird_meta_data_model import DOMAIN
+admin.site.register(DOMAIN)
 from .bird_meta_data_model import FACET_COLLECTION
 admin.site.register(FACET_COLLECTION)
 from .bird_meta_data_model import MAINTENANCE_AGENCY
@@ -30,6 +17,10 @@ from .bird_meta_data_model import MEMBER_HIERARCHY_NODE
 admin.site.register(MEMBER_HIERARCHY_NODE)
 from .bird_meta_data_model import VARIABLE
 admin.site.register(VARIABLE)
+from .bird_meta_data_model import VARIABLE_SET
+admin.site.register(VARIABLE_SET)
+from .bird_meta_data_model import VARIABLE_SET_ENUMERATION
+admin.site.register(VARIABLE_SET_ENUMERATION)
 from .bird_meta_data_model import FRAMEWORK
 admin.site.register(FRAMEWORK)
 from .bird_meta_data_model import MEMBER_MAPPING
@@ -56,12 +47,12 @@ from .bird_meta_data_model import TABLE
 admin.site.register(TABLE)
 from .bird_meta_data_model import TABLE_CELL
 admin.site.register(TABLE_CELL)
-from .bird_meta_data_model import CUBE
-admin.site.register(CUBE)
 from .bird_meta_data_model import CUBE_STRUCTURE
 admin.site.register(CUBE_STRUCTURE)
 from .bird_meta_data_model import CUBE_STRUCTURE_ITEM
 admin.site.register(CUBE_STRUCTURE_ITEM)
+from .bird_meta_data_model import CUBE
+admin.site.register(CUBE)
 from .bird_meta_data_model import CUBE_LINK
 admin.site.register(CUBE_LINK)
 from .bird_meta_data_model import CUBE_STRUCTURE_ITEM_LINK
@@ -72,12 +63,6 @@ from .bird_meta_data_model import COMBINATION_ITEM
 admin.site.register(COMBINATION_ITEM)
 from .bird_meta_data_model import CUBE_TO_COMBINATION
 admin.site.register(CUBE_TO_COMBINATION)
-from .bird_meta_data_model import SUBDOMAIN_ENUMERATION
-admin.site.register(SUBDOMAIN_ENUMERATION)
-from .bird_meta_data_model import VARIABLE_SET
-admin.site.register(VARIABLE_SET)
-from .bird_meta_data_model import VARIABLE_SET_ENUMERATION
-admin.site.register(VARIABLE_SET_ENUMERATION)
 from .bird_meta_data_model import MEMBER_LINK
 admin.site.register(MEMBER_LINK)
 from .bird_meta_data_model import AutomodeConfiguration
