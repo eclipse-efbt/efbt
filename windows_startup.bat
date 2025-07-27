2d4ea923fd4e5c6e16f25868b63c019152edd5f5
RBirdwatcher <neil@birdsoftwaresolutions.com>
2025-07-27 06:19:39 +0100

    Some dependencies are not imported in the winsdows startup batch file #1625
    https://github.com/eclipse-efbt/efbt/issues/1625

--- windows_startup.bat
+++ windows_startup.bat
@@ -8,6 +8,7 @@ python -m pip install pytest-xdist==3.6.1 --quiet
 python -m pip install ruff==0.9.7 --quiet
 python -m pip install unidecode==1.3.8 --quiet
 python -m pip install mssql-django pyodbc --quiet
+python -m pip install pandas --quiet
 python -m pip install numpy==2.2.4 --quiet
 python -m pip install requests==2.32.3 --quiet
 python -m pip install psutil --quiet
