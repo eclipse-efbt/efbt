6e508d9f74e0a6b650d5b87e5cbb7dbc78041ab9
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2025-09-02 10:24:52 +0100

    security check #1656
    https://github.com/eclipse-efbt/efbt/issues/1656

--- birds_nest/birds_nest/settings.py
+++ birds_nest/birds_nest/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0f_te@%1w4g_eniz@tw2b7%n*+3_vt3cg@()u8_r%xqv%isy4s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0f_te@%1w4g_eniz@tw2b7%n*+3_vt3cg@()u8_r%xqv%isy4s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
