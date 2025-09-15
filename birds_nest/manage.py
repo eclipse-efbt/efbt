870892c7ef9f7bb46dd73144db287066f7bbba1d
Benjamin Arfa <benjamin.arfa.pro@gmail.com>
2025-09-15 14:58:24 +0200

    >chores: add licensing and change the packages needed to build the  project (in the pyproject.toml)

--- birds_nest/manage.py
+++ birds_nest/manage.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+# coding=UTF-8
+# Copyright (c) 2025 Bird Software Solutions Ltd
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#    Neil Mackenzie - initial API and implementation
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
